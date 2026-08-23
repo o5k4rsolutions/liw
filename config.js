a883160e6fb549dd09016a34a03c8161de940b00
o5k4rsolutions <227397329+o5k4rsolutions@users.noreply.github.com>
2026-08-23 15:41:14 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+    discordWebhookUrl: "https://discord.com/api/webhooks/1539659755843489964/mGmQNG8g1RMdwx1DO68PYFNKe4AQ3_B1PTTF_Y9kN228x00LDtf8SAp4XIfSnHR9LMBY",
+    emailJsPublicKey: "MvnEcMGrMQXtz5vMa",
+    emailJsServiceId: "service_ydvy0ym",
+    emailJsTemplateId: "template_hv49lmp"
+};
