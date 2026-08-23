2ce1e5166e9725576f42bf9d5fc0030e84d2fa2d
o5k4rsolutions <227397329+o5k4rsolutions@users.noreply.github.com>
2026-08-23 15:45:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-window.APP_CONFIG = {
-    discordWebhookUrl: "https://discord.com/api/webhooks/1539659755843489964/mGmQNG8g1RMdwx1DO68PYFNKe4AQ3_B1PTTF_Y9kN228x00LDtf8SAp4XIfSnHR9LMBY",
-    emailJsPublicKey: "MvnEcMGrMQXtz5vMa",
-    emailJsServiceId: "service_ydvy0ym",
-    emailJsTemplateId: "template_hv49lmp"
+const CONFIG = {
+    EMAILJS_PUBLIC_KEY: "MvnEcMGrMQXtz5vMa",
+    EMAILJS_SERVICE_ID: "service_ydvy0ym",
+    EMAILJS_TEMPLATE_ID: "template_hv49lmp",
+    DISCORD_WEBHOOK_URL: "https://discord.com/api/webhooks/1539659755843489964/mGmQNG8g1RMdwx1DO68PYFNKe4AQ3_B1PTTF_Y9kN228x00LDtf8SAp4XIfSnHR9LMBY"
 };
